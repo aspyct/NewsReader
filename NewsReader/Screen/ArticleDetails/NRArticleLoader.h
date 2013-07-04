384f664863d0563395af857bfd7eef7b380e49c7
Antoine d Otreppe <a.dotreppe@aspyct.org>
2013-07-04 14:50:44 +0200

    Added & implemented the NRArticleLoader

---
+++ NewsReader/Screen/ArticleDetails/NRArticleLoader.h
@@ -0,0 +1,17 @@
+//
+//  NRArticleLoader.h
+//  NewsReader
+//
+//  Created by Antoine d Otreppe on 04/07/13.
+//  Copyright (c) 2013 Aspyct. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "NRArticle.h"
+
+@interface NRArticleLoader : NSObject
+
+- (void)fillArticle:(NRArticle *)article success:(void (^)())success failure:(void (^)())failure;
+
+@end
---
+++ NewsReader/Screen/ArticleDetails/NRArticleLoader.m
@@ -0,0 +1,35 @@
+//
+//  NRArticleLoader.m
+//  NewsReader
+//
+//  Created by Antoine d Otreppe on 04/07/13.
+//  Copyright (c) 2013 Aspyct. All rights reserved.
+//
+
+#import "NRArticleLoader.h"
+
+#import "AFNetworking.h"
+
+@implementation NRArticleLoader
+
+- (void)fillArticle:(NRArticle *)article success:(void (^)())success failure:(void (^)())failure {
+    NSString *urlString = [NSString stringWithFormat:@"http://localhost:8000/article/details/%@", article.articleId];
+    NSURL *url = [NSURL URLWithString:urlString];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    
+    AFJSONRequestOperation *operation = [AFJSONRequestOperation JSONRequestOperationWithRequest:request success:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON) {
+        [article fillWithDict:JSON];
+        
+        if (success) {
+            success();
+        }
+    } failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error, id JSON) {
+        if (failure) {
+            failure();
+        }
+    }];
+    
+    [operation start];
+}
+
+@end
